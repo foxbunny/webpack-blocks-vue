fe31d1290ad2963296238608ec4d622044a1525c
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2017-03-23 22:51:07 +0100

    Initial commit
    Signed-off-by: Hajime Yamasaki Vukelic <hayavuk@gmail.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# Webpack blocks - TypeScript
+
+This is the `vue` block providing [Vue.js](vuejs.org) component support. Uses
+[vue-loader](http://vue-loader.vuejs.org/en/).
+
+## Options
+
+Uses the same options as [advanced vue-loader
+configuratioin](http://vue-loader.vuejs.org/en/configurations/advanced.html),
+with the additon of `extractCSS` boolean flag, which is a shortcut for [extracting CSS](http://vue-loader.vuejs.org/en/configurations/extract-css.html).
+
+## Example:
+
+```javascript
+module.exports = createConfig([
+  // ...
+  vue({
+    loaders: {
+      typescript: 'awesome-typescript-loader',
+      css: 'sass-loader'
+    },
+    extractCSS: true
+  }),
+  // ...
+])
+```
---
+++ test.js
@@ -0,0 +1,67 @@
+var vue = require('./index')
+
+const mockContext = (webpackVersion = 1) => {
+  const ctx = {
+    fileType: jest.fn(),
+    webpackVersion: {
+      major: webpackVersion
+    } 
+  }
+  ctx.fileType.all = jest.fn()
+  ctx.fileType.add = jest.fn()
+  return ctx
+}
+
+test('default options', () => {
+  expect(typeof vue({})).toBe('function')
+})
+
+;[1, 2].forEach((webpackVer) => {
+  test(`default options on webpack ${webpackVer}`, () => {
+    const blockfn = vue({})
+    expect(blockfn(mockContext(webpackVer))).toMatchSnapshot()
+  })
+
+  test(`customize js loader on webpack ${webpackVer}`, () => {
+    const blockfn = vue({loaders: {js: 'ts-loader'}})
+    expect(blockfn(mockContext(webpackVer))).toMatchSnapshot()
+  })
+
+  test(`customize css loader on webpack ${webpackVer}`, () => {
+    const blockfn = vue({loaders: {css: 'sass-loader'}})
+    expect(blockfn(mockContext(webpackVer))).toMatchSnapshot()
+  })
+
+  test(`customize css with extract on webpack ${webpackVer}`, () => {
+    const blockfn = vue({
+      loaders: {css: 'css-loader'}, 
+      extractCSS: true
+    })
+    expect(blockfn(mockContext(webpackVer))).toMatchSnapshot()
+  })
+
+  ;['sass', 'less', 'stylus'].forEach((ext) => {
+    test(`customize css using ${ext} with extract on webpack ${webpackVer}`, () => {
+      const blockfn = vue({
+        loaders: {css: `${ext}-loader`}, 
+        extractCSS: true
+      })
+      expect(blockfn(mockContext(webpackVer))).toMatchSnapshot()
+    })
+
+    test(`customize ${ext} with extract on webpack ${webpackVer}`, () => {
+      const blockfn = vue({
+        loaders: {[ext]: `${ext}-loader`}, 
+        extractCSS: true
+      })
+      expect(blockfn(mockContext(webpackVer))).toMatchSnapshot()
+    })
+  })
+
+  test(`extract css without loader on webpack ${webpackVer}`, () => {
+    const blockfn = vue({
+      extractCSS: true
+    })
+    expect(blockfn(mockContext(webpackVer))).toMatchSnapshot()
+  })
+})
